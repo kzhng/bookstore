fc8356d6d9081b1fe5f5ff0ade83d91f5c9beafc
Kerry <43202840+kzhng@users.noreply.github.com>
2023-01-22 21:53:41 +1100

    Fix book attribute style
    Used data annotations to make ReleaseDate and BookId have appropriate spacing when displayed.

--- MvcBookstore/Models/Book.cs
+++ MvcBookstore/Models/Book.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcBookstore.Models;
 
@@ -6,9 +7,12 @@ public class Book
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+    [Display(Name = "Book ID")]
     public string? BookId { get; set; }
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Category { get; set; }
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 }
