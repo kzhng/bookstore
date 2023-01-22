34948e08fceda27d27014ae5a528bd8c477682dd
Kerry <43202840+kzhng@users.noreply.github.com>
2023-01-22 11:43:06 +1100

    Add project files.

---
+++ MvcBookstore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBookstore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
