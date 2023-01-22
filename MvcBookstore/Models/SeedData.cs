62360cbfd61625a98175f0a04ba85c31932bbf50
Kerry <43202840+kzhng@users.noreply.github.com>
2023-01-22 21:35:48 +1100

    Add database seeder to seed book database with books
    This commit creates a new class SeedData that adds books to the database if it is empty. A seed initialiser is added to program.cs so that the seed method is called.

---
+++ MvcBookstore/Models/SeedData.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcBookstore.Data;
+
+namespace MvcBookstore.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcBookstoreContext(
+                   serviceProvider.GetRequiredService<
+                       DbContextOptions<MvcBookstoreContext>>()))
+        {
+            // Look for any books.
+            if (context.Book.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Book.AddRange(
+                new Book
+                {
+                    Title = "CQRS for Dummies",
+                    BookId = "9b0896fa-3880-4c2e-bfd6-925c87f22878",
+                    ReleaseDate = DateTime.Parse("2019-2-5"),
+                    Category = "Programming",
+                    Price = 80.99M
+                },
+                new Book
+                {
+                    Title = "Visual Studio Tips",
+                    BookId = "0550818d-36ad-4a8d-9c3a-a715bf15de76",
+                    ReleaseDate = DateTime.Parse("2021-1-15"),
+                    Category = "Programming",
+                    Price = 50.04M
+                },
+                new Book
+                {
+                    Title = "NHibernate Cookbook",
+                    BookId = "8e0f11f1-be5c-4dbc-8012-c19ce8cbe8e1",
+                    ReleaseDate = DateTime.Parse("2017-1-27"),
+                    Category = "Programming",
+                    Price = 158.38M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- MvcBookstore/Program.cs
+++ MvcBookstore/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MvcBookstore.Data;
-using Microsoft.Extensions.DependencyInjection;
+using MvcBookstore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcBookstoreContext>(options =>
@@ -18,6 +18,13 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
