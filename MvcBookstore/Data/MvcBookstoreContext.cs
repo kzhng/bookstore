335e31d8ecb90260cfff89238302b4da9ca46497
Kerry <43202840+kzhng@users.noreply.github.com>
2023-01-22 13:39:27 +1100

    Add Book model and Book controller
    A Book model with various fields to be used by the database was implemented. Various packages were installed for database and model use. A scaffolded book controller was added. Multiple books were added onto the database.

---
+++ MvcBookstore/Data/MvcBookstoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcBookstore.Models;
+
+namespace MvcBookstore.Data
+{
+    public class MvcBookstoreContext : DbContext
+    {
+        public MvcBookstoreContext (DbContextOptions<MvcBookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcBookstore.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ MvcBookstore/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcBookstore.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? BookId { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Category { get; set; }
+    public decimal Price { get; set; }
+}
