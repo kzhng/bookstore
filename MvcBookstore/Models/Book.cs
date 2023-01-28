37633e4cc81bd36524e57782893ed831f27fd083
Kerry <43202840+kzhng@users.noreply.github.com>
2023-01-28 18:14:10 +1100

    Add validation rules for the books model
    Validation rules were implemented for attributes of the book model. This is to ensure that the fields entered by the user is not junk.

--- MvcBookstore/Models/Book.cs
+++ MvcBookstore/Models/Book.cs
@@ -6,15 +6,32 @@ namespace MvcBookstore.Models;
 public class Book
 {
     public int Id { get; set; }
+
+    [StringLength(70, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
+
+    [StringLength(100, MinimumLength = 3)]
+    [Required]
     [Display(Name = "Book ID")]
     public string? BookId { get; set; }
+
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Category { get; set; }
+
+    [Range(1, 500)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(10)]
     public string? Status { get; set; } = "Available";
 }
